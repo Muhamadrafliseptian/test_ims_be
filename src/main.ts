3b7e0fa64239f1b40dd718e6af3921daba226f40
Muhamadrafliseptian <rafliseptiannn25@gmail.com>
2025-01-05 13:17:27 +0700

    push

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Aktifkan CORS dan tentukan origin yang diizinkan
   app.enableCors({
-    origin: 'http://localhost:3001',  // Gantilah dengan alamat origin frontend Anda
+    origin: 'https://test-ims-muhamadrafliseptians-projects.vercel.app', 
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Accept',
   });
 
-  // Menentukan port aplikasi
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
